bdb89ff8a5579953edf10c2e94b3253831add037
ramg86929137 <rambabu.gupta@neosoftmail.com>
2024-01-17 14:59:59 +0530

    model and save methods explained

---
+++ app/Http/Controllers/FlightController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FlightController extends Controller
+{
+    
+    public function store(Request $request){
+
+        //
+        // $student = new Student;
+        // $student->student_name = $request->student_name;
+        // $student->student_email = $request->student_email;
+        // $student->save();
+
+        //by using create method we can use fillable or guard for mass assignment
+        // $data = [
+        //     'student_name' => $request->student_name,
+        //     'student_email' => $request->student_email,
+        // ];
+        // Student::create($data);
+
+    }
+}
